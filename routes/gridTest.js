6f1ba242897154fb52fa40037be8b899375d2064
devmrko <devmrko@gmail.com>
2016-03-04 17:29:45 +0900

    pagination developing...

--- routes/gridTest.js
+++ routes/gridTest.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var perPage = 2;
 
 /* GET home page. */
 router.get('/list', function (req, res, next) {
@@ -14,15 +15,22 @@ var customRenderer = function (res, titleStr, dataObjNm, dataObj) {
 }
 
 router.get('/', function (req, res, next) {
+
+    var pageNo = req.body.page_no;
+    if (pageNo == undefined || pageNo == '') {
+        pageNo = 0;
+    }
+
     var db = req.db;
     var test_cols = db.get('memo');
 
-    test_cols.find({}, {}, function (err, test_cols) {
+    test_cols.find({}, {skip: pageNo * perPage, limit: perPage}, function (err, test_cols) {
         // return customRenderer(res, 'Grid Test', 'test_cols', test_cols);
         return res.render('gridTest', {
             "title": 'Grid Test',
             'test_cols': test_cols
         });
+        //.skip().limit(perPage);
     });
 
 });
@@ -31,28 +39,42 @@ router.get('/search', function (req, res, next) {
     res.send('search get request called');
 });
 
+router.post('/next', function (req, res, next) {
+    req.body.page_no = req.body.page_no + 1;
+    res.location('/gridTest');
+    res.redirect('/gridTest');
+});
+
 router.post('/search', function (req, res, next) {
-    // get form values
-    var searchText = req.body.id_searchText;
     
-    // // foam validation
-    // req.checkBody('id_searchText', 'Search field is required').notEmpty();
-    // // Check Errors
-    // var errors = req.validationErrors();
-    // if (errors) {
-    //     // some error handling codes
-    // } else {
+    var pageNo = req.body.page_no;
+    if (pageNo == undefined || pageNo == '') {
+        pageNo = 0;
+    }
+    
+        // get form values
+        var searchText = req.body.id_searchText;
+    
+        // // foam validation
+        // req.checkBody('id_searchText', 'Search field is required').notEmpty();
+        // // Check Errors
+        // var errors = req.validationErrors();
+        // if (errors) {
+        //     // some error handling codes
+        // } else {
         var db = req.db;
         var test_cols = db.get('memo');
 
-        test_cols.find({ "contents": { "$regex": searchText } }, {}, function (err, test_cols) {
-            res.render('gridTest', {
-                "title": 'Grid Test',
-                "test_cols": test_cols
-            });
+        test_cols.find({ "contents": { "$regex": searchText } }, {skip: pageNo * perPage, limit: perPage}, 
+            function (err, test_cols) {
+                res.render('gridTest', {
+                    "title": 'Grid Test',
+                    "test_cols": test_cols
+                });
         });
-    // }
-});
+        //.skip(pageNo * perPage).limit(perPage);;
+        // }
+    });
 
 
 router.post('/save', function (req, res, next) {
@@ -68,45 +90,45 @@ router.post('/save', function (req, res, next) {
     // if (errors) {
     //     // some error handling codes
     // } else {
-        var db = req.db;
-        var test_cols = db.get('memo');
-        
-        if(selId == '') {
-            test_cols.insert({
-                "contents": selContents,
-                "tags": selTags
-            }, function(err, test_cols){
-                if(err){
-                    res.send('There was an issue submitting the post');
+    var db = req.db;
+    var test_cols = db.get('memo');
+
+    if (selId == '') {
+        test_cols.insert({
+            "contents": selContents,
+            "tags": selTags
+        }, function (err, test_cols) {
+            if (err) {
+                res.send('There was an issue submitting the post');
+            } else {
+                req.flash('success', 'Post Submitted');
+                res.location('/gridTest');
+                res.redirect('/gridTest');
+            }
+        });
+    } else {
+        test_cols.update({
+            "_id": selId
+        },
+            {
+                $set: {
+                    "contents": selContents,
+                    "tags": selTags
+                }
+            },
+            function (err, doc) {
+                if (err) {
+                    throw err;
                 } else {
-                    req.flash('success','Post Submitted');
+                    req.flash('success', 'Comment Added');
+                    // res.location('/posts/show/'+selId);
+                    // res.redirect('/posts/show/'+selId);
                     res.location('/gridTest');
                     res.redirect('/gridTest');
                 }
-            });
-        } else {
-            test_cols.update({
-                    "_id": selId
-                },
-                {
-                    $set:{
-                        "contents": selContents,
-                        "tags": selTags
-                    }
-                },
-                function(err, doc){
-                    if(err){
-                        throw err;
-                    } else {
-                        req.flash('success','Comment Added');
-                        // res.location('/posts/show/'+selId);
-                        // res.redirect('/posts/show/'+selId);
-                        res.location('/gridTest');
-                        res.redirect('/gridTest');
-                    }
-                }
+            }
             );
-        }
+    }
     // }
 });
 
